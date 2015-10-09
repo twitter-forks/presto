c76b42e910bfbf989078171587dc6700f2757275
Sailesh Mittal <smittal@twitter.com>
2015-10-09 12:43:09 -0700

    Revert "Reduce allocation of hadoop configuration objects"
    This reverts commit 7365f1ad3a75b478bcdecca65676d7857b843699.
    This blocked us from reading from mountable clusters.

--- presto-hive/src/main/java/com/facebook/presto/hive/HiveHdfsConfiguration.java
+++ presto-hive/src/main/java/com/facebook/presto/hive/HiveHdfsConfiguration.java
@@ -18,25 +18,19 @@ import org.apache.hadoop.conf.Configuration;
 import javax.inject.Inject;
 
 import java.net.URI;
-import java.util.Map;
 
 import static java.util.Objects.requireNonNull;
 
 public class HiveHdfsConfiguration
         implements HdfsConfiguration
 {
-    private static final Configuration DEFAULT_CONFIGURATION = new Configuration();
-
     @SuppressWarnings("ThreadLocalNotStaticFinal")
     private final ThreadLocal<Configuration> hadoopConfiguration = new ThreadLocal<Configuration>()
     {
         @Override
         protected Configuration initialValue()
         {
-            Configuration config = new Configuration(false);
-            for (Map.Entry<String, String> entry : DEFAULT_CONFIGURATION) {
-                config.set(entry.getKey(), entry.getValue());
-            }
+            Configuration config = new Configuration();
             updater.updateConfiguration(config);
             return config;
         }
