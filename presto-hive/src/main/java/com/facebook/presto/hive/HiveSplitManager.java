344c8f0b4e90e38a26f443b9ad090f632ae54478
Bill Graham <billg@twitter.com>
2015-12-08 11:19:49 -0800

    HiveSplitManager to start HiveSplitLoader as session user

--- presto-hive/src/main/java/com/facebook/presto/hive/HiveSplitManager.java
+++ presto-hive/src/main/java/com/facebook/presto/hive/HiveSplitManager.java
@@ -34,9 +34,12 @@ import org.apache.hadoop.hive.metastore.ProtectMode;
 import org.apache.hadoop.hive.metastore.api.FieldSchema;
 import org.apache.hadoop.hive.metastore.api.Partition;
 import org.apache.hadoop.hive.metastore.api.Table;
+import org.apache.hadoop.security.UserGroupInformation;
 
 import javax.inject.Inject;
 
+import java.io.IOException;
+import java.security.PrivilegedExceptionAction;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -180,9 +183,22 @@ public class HiveSplitManager
                 recursiveDfsWalkerEnabled);
 
         HiveSplitSource splitSource = new HiveSplitSource(connectorId, maxOutstandingSplits, hiveSplitLoader, executor);
-        hiveSplitLoader.start(splitSource);
 
-        return splitSource;
+        UserGroupInformation ugi = UserGroupInformation.createRemoteUser(session.getUser());
+
+        try {
+          ugi.doAs((PrivilegedExceptionAction<Void>)
+            () -> {
+              hiveSplitLoader.start(splitSource);
+              return null;
+            }
+          );
+        }
+        catch (IOException | InterruptedException e) {
+          throw new RuntimeException("Could not runAs " + ugi.getUserName(), e);
+        }
+
+      return splitSource;
     }
 
     private Iterable<HivePartitionMetadata> getPartitionMetadata(Table table, SchemaTableName tableName, List<HivePartition> hivePartitions)
