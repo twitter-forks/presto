48973308e108c13354c5c7b33823584fd32cad90
Bill Graham <billg@twitter.com>
2016-01-12 10:38:13 -0800

    Cache the UGI object per user in BackgroundHiveSplitLoader

--- presto-hive/src/main/java/com/facebook/presto/hive/BackgroundHiveSplitLoader.java
+++ presto-hive/src/main/java/com/facebook/presto/hive/BackgroundHiveSplitLoader.java
@@ -51,6 +51,7 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.Properties;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedDeque;
 import java.util.concurrent.Executor;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -71,6 +72,11 @@ public class BackgroundHiveSplitLoader
         implements HiveSplitLoader
 {
     public static final CompletableFuture<?> COMPLETED_FUTURE = CompletableFuture.completedFuture(null);
+
+    // Every instance of a UserGroupInformation object for a given user has a unique hashcode, due
+    // to the hashCode() impl. If we don't cache the UGI per-user here, there will be a memory leak
+    // in the PrestoFileSystemCache.
+    private static final Map<String, UserGroupInformation> UGI_CACHE = new ConcurrentHashMap<>();
 
     private final String connectorId;
     private final Table table;
@@ -140,8 +146,26 @@ public class BackgroundHiveSplitLoader
     public void start(HiveSplitSource splitSource)
     {
         this.hiveSplitSource = splitSource;
+
+        UserGroupInformation ugi = null;
+
+        if (HiveSessionProperties.getReadAsQueryUser(session)) {
+            String user = session.getUser();
+            ugi = UGI_CACHE.get(user);
+
+            if (ugi == null) {
+                // TODO: Configure hadoop to allow presto daemon user to impersonate all presto users
+                // (HADOOPINFRA-7081) and then change to the approach below (IQ-85).
+                // See https://hadoop.apache.org/docs/r2.4.1/hadoop-project-dist/hadoop-common/Superusers.html
+                // UserGroupInformation ugi = UserGroupInformation.createProxyUser(
+                //        session.getUser(), UserGroupInformation.getLoginUser());
+                ugi = UserGroupInformation.createRemoteUser(user);
+                UGI_CACHE.put(user, ugi);
+            }
+        }
+
         for (int i = 0; i < maxPartitionBatchSize; i++) {
-            ResumableTasks.submit(executor, new HiveSplitLoaderTask());
+            ResumableTasks.submit(executor, new HiveSplitLoaderTask(ugi));
         }
     }
 
@@ -154,18 +178,18 @@ public class BackgroundHiveSplitLoader
     private class HiveSplitLoaderTask
             implements ResumableTask
     {
+        private UserGroupInformation ugi;
+
+        public HiveSplitLoaderTask(UserGroupInformation ugi)
+        {
+            this.ugi = ugi;
+        }
+
         @Override
         public TaskStatus process()
         {
-            if (HiveSessionProperties.getReadAsQueryUser(session)) {
+            if (ugi != null) {
                 try {
-                    // TODO: Configure hadoop to allow presto daemon user to impersonate all presto users
-                    // (HADOOPINFRA-7081) and then change to the approach below (IQ-85).
-                    // See https://hadoop.apache.org/docs/r2.4.1/hadoop-project-dist/hadoop-common/Superusers.html
-                    // UserGroupInformation ugi = UserGroupInformation.createProxyUser(
-                    //        session.getUser(), UserGroupInformation.getLoginUser());
-                    UserGroupInformation ugi = UserGroupInformation.createRemoteUser(session.getUser());
-
                     return ugi.doAs((PrivilegedExceptionAction<TaskStatus>) this::doProcess);
                 }
                 catch (IOException | InterruptedException e) {
