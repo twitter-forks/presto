d2b4b3bd50e6d451eec7917aea3cd298c6323bd6
Bill Graham <billg@twitter.com>
2016-03-22 15:10:42 -0700

    Patch partition filters and logging

--- presto-hive/src/main/java/com/facebook/presto/hive/HiveMetadata.java
+++ presto-hive/src/main/java/com/facebook/presto/hive/HiveMetadata.java
@@ -1244,11 +1244,15 @@ public class HiveMetadata
 
         HivePartitionResult hivePartitionResult = partitionManager.getPartitions(session, metastore, tableHandle, constraint.getSummary());
 
+        List<HivePartition> partitions = hivePartitionResult.getPartitions().stream()
+                .filter(partition -> constraint.predicate().test(partition.getKeys()))
+                .collect(toList());
+
         return ImmutableList.of(new ConnectorTableLayoutResult(
                 getTableLayout(session, new HiveTableLayoutHandle(
                         handle.getClientId(),
                         ImmutableList.copyOf(hivePartitionResult.getPartitionColumns()),
-                        hivePartitionResult.getPartitions(),
+                        partitions,
                         hivePartitionResult.getEnforcedConstraint())),
                 hivePartitionResult.getUnenforcedConstraint()));
     }
