dbb4c42e330e42815a097da727f26cad68d55141
Bill Graham <billg@twitter.com>
2016-03-29 12:29:07 -0700

    Add event process and handlers and scribe query completion events

--- presto-main/src/main/java/com/facebook/presto/twitter/TwitterModuleLoader.java
+++ presto-main/src/main/java/com/facebook/presto/twitter/TwitterModuleLoader.java
@@ -13,10 +13,17 @@
  */
 package com.facebook.presto.twitter;
 
+import com.facebook.presto.event.EventProcessor;
+import com.facebook.presto.event.query.QueryCompletionEvent;
+import com.facebook.presto.event.query.QueryCreatedEvent;
+import com.facebook.presto.event.query.QueryEventHandler;
+import com.facebook.presto.event.query.SplitCompletionEvent;
 import com.facebook.presto.twitter.logging.QueryLogger;
+import com.facebook.presto.twitter.logging.QueryScriber;
 import com.google.common.collect.ImmutableList;
 import com.google.inject.Module;
 import com.google.inject.Scopes;
+import com.google.inject.TypeLiteral;
 import com.google.inject.multibindings.Multibinder;
 import io.airlift.event.client.EventClient;
 
@@ -37,10 +44,21 @@ public class TwitterModuleLoader
 
     public static Iterable<? extends Module> getAdditionalModules()
     {
-        return ImmutableList.of(binder ->
-                Multibinder.newSetBinder(binder, EventClient.class)
+        return ImmutableList.of(
+                binder -> Multibinder.newSetBinder(binder, EventClient.class)
                         .addBinding()
-                        .to(QueryLogger.class)
-                        .in(Scopes.SINGLETON));
+                        .to(EventProcessor.class)
+                        .in(Scopes.SINGLETON),
+                binder -> Multibinder.newSetBinder(binder, new TypeLiteral<QueryEventHandler<SplitCompletionEvent>>() {}),
+                binder -> Multibinder.newSetBinder(binder, new TypeLiteral<QueryEventHandler<QueryCreatedEvent>>() {}),
+                binder -> Multibinder.newSetBinder(binder, new TypeLiteral<QueryEventHandler<QueryCompletionEvent>>(){})
+                        .addBinding()
+                        .to(new TypeLiteral<QueryLogger>(){})
+                        .in(Scopes.SINGLETON),
+                binder -> Multibinder.newSetBinder(binder, new TypeLiteral<QueryEventHandler<QueryCompletionEvent>>(){})
+                        .addBinding()
+                        .to(QueryScriber.class)
+                        .in(Scopes.SINGLETON)
+                );
     }
 }
