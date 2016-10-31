dee633688c4aa2c49da0fa7c3549d22087eb54b1
Yaliang Wang <yaliangw@twitter.com>
2016-10-31 15:45:52 -0700

    Used RuntimeException to replace PrestoException, prevent whitelist stops RetryDriver

--- presto-hive/src/main/java/com/facebook/presto/twitter/hive/ZookeeperServersetHiveCluster.java
+++ presto-hive/src/main/java/com/facebook/presto/twitter/hive/ZookeeperServersetHiveCluster.java
@@ -16,7 +16,6 @@ package com.facebook.presto.twitter.hive;
 import com.facebook.presto.hive.HiveCluster;
 import com.facebook.presto.hive.HiveMetastoreClientFactory;
 import com.facebook.presto.hive.metastore.HiveMetastoreClient;
-import com.facebook.presto.spi.PrestoException;
 import com.google.common.net.HostAndPort;
 import io.airlift.log.Logger;
 import org.apache.thrift.transport.TTransportException;
@@ -26,7 +25,6 @@ import javax.inject.Inject;
 import java.util.Collections;
 import java.util.List;
 
-import static com.facebook.presto.hive.HiveErrorCode.HIVE_METASTORE_ERROR;
 import static java.util.Objects.requireNonNull;
 
 public class ZookeeperServersetHiveCluster
@@ -65,6 +63,6 @@ public class ZookeeperServersetHiveCluster
             }
         }
 
-        throw new PrestoException(HIVE_METASTORE_ERROR, "Failed connecting to Hive metastore: " + metastores, lastException);
+        throw new RuntimeException("Failed connecting to Hive metastore: " + metastores, lastException);
     }
 }
