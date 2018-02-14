4d02608cf2cb8fef2ad126add2cc96d03b9e3ca9
Hao Luo <hluo@twitter.com>
2018-02-13 17:44:20 -0800

    Using Hadoop-LZO codec in ParquetCompressionUtils

--- presto-hive/src/main/java/com/facebook/presto/hive/parquet/ParquetCompressionUtils.java
+++ presto-hive/src/main/java/com/facebook/presto/hive/parquet/ParquetCompressionUtils.java
@@ -13,20 +13,25 @@
  */
 package com.facebook.presto.hive.parquet;
 
+import com.hadoop.compression.lzo.LzoCodec;
 import io.airlift.compress.Decompressor;
-import io.airlift.compress.lzo.LzoDecompressor;
 import io.airlift.compress.snappy.SnappyDecompressor;
 import io.airlift.slice.DynamicSliceOutput;
 import io.airlift.slice.Slice;
+import org.apache.hadoop.conf.Configuration;
+import parquet.bytes.BytesInput;
 import parquet.hadoop.metadata.CompressionCodecName;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
+import java.nio.channels.WritableByteChannel;
 import java.util.zip.GZIPInputStream;
 
 import static com.google.common.base.Preconditions.checkArgument;
-import static io.airlift.slice.SizeOf.SIZE_OF_INT;
-import static io.airlift.slice.SizeOf.SIZE_OF_LONG;
 import static io.airlift.slice.Slices.EMPTY_SLICE;
 import static io.airlift.slice.Slices.wrappedBuffer;
 import static java.util.Objects.requireNonNull;
@@ -86,30 +91,25 @@ public final class ParquetCompressionUtils
         }
     }
 
+    // decompress LZO-compressed data using HADOOP-LZO codec
     private static Slice decompressLZO(Slice input, int uncompressedSize)
+            throws IOException
     {
-        LzoDecompressor lzoDecompressor = new LzoDecompressor();
-        long totalDecompressedCount = 0;
-        // over allocate buffer which makes decompression easier
-        byte[] output = new byte[uncompressedSize + SIZE_OF_LONG];
-        int outputOffset = 0;
-        int inputOffset = 0;
-        int cumulativeUncompressedBlockLength = 0;
+        final BytesInput decompressed;
+        LzoCodec codec = new LzoCodec();
+        codec.setConf(new Configuration());
+        org.apache.hadoop.io.compress.Decompressor decompressor = codec.createDecompressor();
 
-        while (totalDecompressedCount < uncompressedSize) {
-            if (totalDecompressedCount == cumulativeUncompressedBlockLength) {
-                cumulativeUncompressedBlockLength += Integer.reverseBytes(input.getInt(inputOffset));
-                inputOffset += SIZE_OF_INT;
-            }
-            int compressedChunkLength = Integer.reverseBytes(input.getInt(inputOffset));
-            inputOffset += SIZE_OF_INT;
-            int decompressionSize = decompress(lzoDecompressor, input, inputOffset, compressedChunkLength, output, outputOffset);
-            totalDecompressedCount += decompressionSize;
-            outputOffset += decompressionSize;
-            inputOffset += compressedChunkLength;
-        }
-        checkArgument(outputOffset == uncompressedSize);
-        return wrappedBuffer(output, 0, uncompressedSize);
+        // input data
+        byte[] byteArray = (byte[]) input.getBase();
+        int inputOffset = (int)input.getAddress() - ARRAY_BYTE_BASE_OFFSET;
+        ByteArrayBytesInput bytes = new ByteArrayBytesInput(byteArray, inputOffset, input.length());
+
+        decompressor.reset();
+        InputStream is = codec.createInputStream(toInputStream(bytes), decompressor);
+        decompressed = BytesInput.from(is, uncompressedSize);
+        checkArgument(decompressed.size() == uncompressedSize);
+        return wrappedBuffer(decompressed.toByteArray(), 0, uncompressedSize);
     }
 
     private static int decompress(Decompressor decompressor, Slice input, int inputOffset, int inputLength, byte[] output, int outputOffset)
@@ -118,5 +118,68 @@ public final class ParquetCompressionUtils
         int byteArrayOffset = inputOffset + (int) (input.getAddress() - ARRAY_BYTE_BASE_OFFSET);
         int size = decompressor.decompress(byteArray, byteArrayOffset, inputLength, output, outputOffset, output.length - outputOffset);
         return size;
+    }
+
+    // helper class and helper functions from org.apache.parquet.bytes
+    private static class ByteArrayBytesInput
+            extends BytesInput
+    {
+        private final byte[] in;
+        private final int offset;
+        private final int length;
+
+        private ByteArrayBytesInput(byte[] in, int offset, int length)
+        {
+            this.in = in;
+            this.offset = offset;
+            this.length = length;
+        }
+
+        @Override
+        public void writeAllTo(OutputStream out)
+                throws IOException
+        {
+            out.write(in, offset, length);
+        }
+
+        @Override
+        public long size()
+        {
+            return length;
+        }
+    }
+
+    private static final class BAOS
+            extends ByteArrayOutputStream
+    {
+        private BAOS(int size)
+        {
+            super(size);
+        }
+
+        public byte[] getBuf()
+        {
+            return this.buf;
+        }
+    }
+
+    private static byte[] toByteArray(BytesInput bytes)
+            throws IOException
+    {
+        BAOS baos = new BAOS((int) bytes.size());
+        bytes.writeAllTo(baos);
+        return baos.getBuf();
+    }
+
+    private static InputStream toInputStream(BytesInput bytes)
+            throws IOException
+    {
+        return new ByteBufferInputStream(toByteBuffer(bytes));
+    }
+
+    private static ByteBuffer toByteBuffer(BytesInput bytes)
+            throws IOException
+    {
+        return ByteBuffer.wrap(toByteArray(bytes));
     }
 }
