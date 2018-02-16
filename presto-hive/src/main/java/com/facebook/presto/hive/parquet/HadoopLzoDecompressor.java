ed70595adf161730d32667fbe95bac487a7000ea
Hao Luo <hluo@twitter.com>
2018-02-16 11:31:02 -0800

    Use HadoopLzoDecompressor in ParquetCompressionUtils

---
+++ presto-hive/src/main/java/com/facebook/presto/hive/parquet/HadoopLzoDecompressor.java
@@ -0,0 +1,66 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.facebook.presto.hive.parquet;
+
+import com.hadoop.compression.lzo.LzoCodec;
+import io.airlift.compress.Decompressor;
+import io.airlift.compress.MalformedInputException;
+import org.apache.hadoop.conf.Configuration;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+// HadoopLzoDecompressor from aircompressor
+public class HadoopLzoDecompressor
+        implements Decompressor
+{
+    private static final Configuration HADOOP_CONF = new Configuration();
+
+    private final org.apache.hadoop.io.compress.Decompressor decompressor;
+
+    public HadoopLzoDecompressor()
+    {
+        LzoCodec codec = new LzoCodec();
+        codec.setConf(HADOOP_CONF);
+        decompressor = codec.createDecompressor();
+    }
+
+    @Override
+    public int decompress(byte[] input, int inputOffset, int inputLength, byte[] output, int outputOffset, int maxOutputLength)
+            throws MalformedInputException
+    {
+        decompressor.setInput(input, inputOffset, inputLength);
+
+        int offset = outputOffset;
+        int outputLimit = outputOffset + maxOutputLength;
+        while (!decompressor.finished() && offset < outputLimit) {
+            try {
+                offset += decompressor.decompress(output, offset, outputLimit - offset);
+            }
+            catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        return offset - outputOffset;
+    }
+
+    @Override
+    public void decompress(ByteBuffer input, ByteBuffer output)
+            throws MalformedInputException
+    {
+        throw new UnsupportedOperationException("Not supported in HadoopLzoDecompressor");
+    }
+}
