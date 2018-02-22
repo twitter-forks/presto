63cdd6412d2bdaba58b87fb82f6663234bc723f0
Hao Luo <hluo@twitter.com>
2018-02-21 18:16:10 -0800

    refactor findFieldIndexByName()

--- presto-hive/src/main/java/com/facebook/presto/hive/parquet/ParquetTypeUtils.java
+++ presto-hive/src/main/java/com/facebook/presto/hive/parquet/ParquetTypeUtils.java
@@ -172,30 +172,16 @@ public final class ParquetTypeUtils
     // Find the column index by name following the same logic as findParquetTypeByName
     public static int findFieldIndexByName(MessageType fileSchema, String name)
     {
-        try {
-            return fileSchema.getFieldIndex(name);
-        }
-        catch (InvalidRecordException e) {
-            for (parquet.schema.Type type : fileSchema.getFields()) {
-                if (type.getName().equalsIgnoreCase(name)) {
-                    return fileSchema.getFieldIndex(type.getName());
-                }
-            }
+        // direct match and case-insensitive match
+        int fieldIndex = getFieldIndex(fileSchema, name);
 
-            // when a parquet field is a hive keyword we append an _ to it in hive. When doing
-            // a name-based lookup, we need to strip it off again if we didn't get a direct match.
-            if (name.endsWith("_")) {
-                String alternativeName = name.substring(0, name.length() - 1);
-                for (parquet.schema.Type type : fileSchema.getFields()) {
-                    if (type.getName().equalsIgnoreCase(alternativeName)) {
-                        return fileSchema.getFieldIndex(type.getName());
-                    }
-                }
-            }
-
-            // field not found
-            return -1;
+        // when a parquet field is a hive keyword we append an _ to it in hive.
+        // try remove _ and direct match / case-insensitive match again
+        if (fieldIndex == -1 && name.endsWith("_")) {
+            fieldIndex = getFieldIndex(fileSchema, name.substring(0, name.length() - 1));
         }
+
+        return fieldIndex;
     }
 
     public static ParquetEncoding getParquetEncoding(Encoding encoding)
