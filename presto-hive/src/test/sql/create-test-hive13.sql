baa68cfc35de2d061136e334feee41bde22ad18b
Yaliang Wang <yaliangw@twitter.com>
2018-03-27 13:39:25 -0700

    Fix test failure due to twitter patch in sql

--- presto-hive/src/test/sql/create-test-hive13.sql
+++ presto-hive/src/test/sql/create-test-hive13.sql
@@ -104,12 +104,10 @@ CREATE TABLE presto_test_types_parquet (
 , t_struct STRUCT<s_string: STRING, s_double:DOUBLE>
 , t_complex MAP<INT, ARRAY<STRUCT<s_string: STRING, s_double:DOUBLE>>>
 )
-PARTITIONED BY (dummy INT)
 STORED AS PARQUET
 ;
 
 INSERT INTO TABLE presto_test_types_parquet
-PARTITION (dummy=0)
 SELECT
   t_string
 , t_varchar
@@ -130,8 +128,6 @@ SELECT
 FROM presto_test_types_textfile
 ;
 
-ALTER TABLE presto_test_types_parquet
-CHANGE COLUMN t_struct t_struct STRUCT<s_string:STRING, s_double:DOUBLE, s_boolean:BOOLEAN>;
 
 ALTER TABLE presto_test_types_textfile ADD COLUMNS (new_column INT);
 ALTER TABLE presto_test_types_sequencefile ADD COLUMNS (new_column INT);
