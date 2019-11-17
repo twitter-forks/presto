6247b1482cbfa8de8ccce47389c96b2bca60b608
Da Cheng <bjslax@gmail.com>
2019-11-16 18:55:57 -0800

    Port event listener for query logging (#225)

---
+++ twitter-eventlistener-plugin/src/main/java/com/twitter/presto/plugin/eventlistener/TwitterEventListenerFactory.java
@@ -0,0 +1,57 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.twitter.presto.plugin.eventlistener;
+
+import com.facebook.presto.spi.eventlistener.EventListener;
+import com.facebook.presto.spi.eventlistener.EventListenerFactory;
+import com.google.inject.Injector;
+import io.airlift.bootstrap.Bootstrap;
+
+import java.util.Map;
+
+import static com.google.common.base.Throwables.throwIfUnchecked;
+
+public class TwitterEventListenerFactory
+        implements EventListenerFactory
+{
+    @Override
+    public String getName()
+    {
+        return "twitter-event-listener";
+    }
+
+    @Override
+    public EventListener create(Map<String, String> config)
+    {
+        try {
+            Bootstrap app = new Bootstrap(new TwitterEventListenerModule());
+
+            Injector injector = app
+                    .strictConfig()
+                    .doNotInitializeLogging()
+                    .setRequiredConfigurationProperties(config)
+                    .initialize();
+
+            return injector.getInstance(TwitterEventListener.class);
+        }
+        catch (InterruptedException ie) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException("Interrupted while creating connector", ie);
+        }
+        catch (Exception e) {
+            throwIfUnchecked(e);
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ twitter-eventlistener-plugin/src/main/java/com/twitter/presto/plugin/eventlistener/scriber/TwitterScriber.java
@@ -0,0 +1,72 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.twitter.presto.plugin.eventlistener.scriber;
+
+import com.twitter.logging.BareFormatter$;
+import com.twitter.logging.Level;
+import com.twitter.logging.QueueingHandler;
+import com.twitter.logging.ScribeHandler;
+import org.apache.thrift.TBase;
+import org.apache.thrift.TException;
+import org.apache.thrift.TSerializer;
+
+import java.util.Base64;
+import java.util.logging.LogRecord;
+
+public class TwitterScriber
+{
+    private static final int MAX_QUEUE_SIZE = 1000;
+
+    private QueueingHandler queueingHandler;
+
+    // TSerializer is not thread safe
+    private final ThreadLocal<TSerializer> serializer = ThreadLocal.withInitial(() -> new TSerializer());
+
+    public TwitterScriber(String scribeCategory)
+    {
+        ScribeHandler scribeHandler = new ScribeHandler(
+                ScribeHandler.DefaultHostname(),
+                ScribeHandler.DefaultPort(),
+                scribeCategory,
+                ScribeHandler.DefaultBufferTime(),
+                ScribeHandler.DefaultConnectBackoff(),
+                ScribeHandler.DefaultMaxMessagesPerTransaction(),
+                ScribeHandler.DefaultMaxMessagesToBuffer(),
+                BareFormatter$.MODULE$,
+                scala.Option.apply(null));
+        queueingHandler = new QueueingHandler(scribeHandler, MAX_QUEUE_SIZE);
+    }
+
+    public void scribe(TBase thriftMessage)
+            throws TException
+    {
+        scribe(serializeThriftToString(thriftMessage));
+    }
+
+    /**
+     * Serialize a thrift object to bytes, compress, then encode as a base64 string.
+     * Throws TException
+     */
+    private String serializeThriftToString(TBase thriftMessage)
+            throws TException
+    {
+        return Base64.getEncoder().encodeToString(serializer.get().serialize(thriftMessage));
+    }
+
+    protected void scribe(String message)
+    {
+        LogRecord logRecord = new LogRecord(Level.ALL, message);
+        queueingHandler.publish(logRecord);
+    }
+}
