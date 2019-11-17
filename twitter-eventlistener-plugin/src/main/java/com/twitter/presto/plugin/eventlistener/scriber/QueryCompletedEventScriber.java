e98f3e98533aca9c944181007757cf8e41d43c07
Da Cheng <bjslax@gmail.com>
2019-11-16 18:55:57 -0800

    Port event listener for query logging (#225)

---
+++ twitter-eventlistener-plugin/src/main/java/com/twitter/presto/plugin/eventlistener/scriber/QueryCompletedEventScriber.java
@@ -0,0 +1,131 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.twitter.presto.plugin.eventlistener.scriber;
+
+import com.facebook.presto.spi.eventlistener.QueryCompletedEvent;
+import com.facebook.presto.spi.eventlistener.QueryContext;
+import com.facebook.presto.spi.eventlistener.QueryFailureInfo;
+import com.facebook.presto.spi.eventlistener.QueryMetadata;
+import com.facebook.presto.spi.eventlistener.QueryStatistics;
+import com.twitter.presto.plugin.eventlistener.TwitterEventHandler;
+import com.twitter.presto.plugin.eventlistener.TwitterEventListenerConfig;
+import com.twitter.presto.thriftjava.QueryCompletionEvent;
+import com.twitter.presto.thriftjava.QueryState;
+import io.airlift.log.Logger;
+import org.apache.thrift.TException;
+
+import javax.inject.Inject;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * Class that scribes query completion events
+ */
+public class QueryCompletedEventScriber
+        implements TwitterEventHandler
+{
+    private static final String DASH = "-";
+    private static final Logger log = Logger.get(QueryCompletedEventScriber.class);
+
+    private final TwitterScriber scriber;
+
+    @Inject
+    public QueryCompletedEventScriber(TwitterEventListenerConfig config)
+    {
+        requireNonNull(config.getScribeCategory(), "scribe category is null");
+        this.scriber = new TwitterScriber(config.getScribeCategory());
+    }
+
+    public QueryCompletedEventScriber(TwitterScriber scriber)
+    {
+        this.scriber = requireNonNull(scriber, "scriber is null");
+    }
+
+    @Override
+    public void handleQueryCompleted(QueryCompletedEvent event)
+    {
+        try {
+            scriber.scribe(toThriftQueryCompletionEvent(event));
+        }
+        catch (TException e) {
+            log.warn(e,
+                    String.format("Could not serialize thrift object of Query(id=%s, user=%s, env=%s, schema=%s.%s)",
+                            event.getMetadata().getQueryId(),
+                            event.getContext().getUser(),
+                            event.getContext().getEnvironment(),
+                            event.getContext().getCatalog().orElse(DASH),
+                            event.getContext().getSchema().orElse(DASH)));
+        }
+    }
+
+    private static QueryCompletionEvent toThriftQueryCompletionEvent(QueryCompletedEvent event)
+    {
+        QueryMetadata eventMetadata = event.getMetadata();
+        QueryContext eventContext = event.getContext();
+        QueryStatistics eventStat = event.getStatistics();
+
+        QueryCompletionEvent thriftEvent =
+                new com.twitter.presto.thriftjava.QueryCompletionEvent();
+
+        thriftEvent.setQuery_id(eventMetadata.getQueryId());
+        thriftEvent.setTransaction_id(eventMetadata.getTransactionId().orElse(DASH));
+        thriftEvent.setUser(eventContext.getUser());
+        thriftEvent.setPrincipal(eventContext.getPrincipal().orElse(DASH));
+        thriftEvent.setSource(eventContext.getSource().orElse(DASH));
+        thriftEvent.setServer_version(eventContext.getServerVersion());
+        thriftEvent.setEnvironment(eventContext.getEnvironment());
+        thriftEvent.setCatalog(eventContext.getCatalog().orElse(DASH));
+        thriftEvent.setSchema(eventContext.getSchema().orElse(DASH));
+        Map<String, List<String>> queriedColumnsByTable = new HashMap<>();
+        event.getIoMetadata().getInputs().forEach(input -> queriedColumnsByTable.put(String.format("%s.%s", input.getSchema(), input.getTable()), input.getColumns()));
+        thriftEvent.setQueried_columns_by_table(queriedColumnsByTable);
+        thriftEvent.setRemote_client_address(eventContext.getRemoteClientAddress().orElse(DASH));
+        thriftEvent.setUser_agent(eventContext.getUserAgent().orElse(DASH));
+        thriftEvent.setQuery_state(QueryState.valueOf(eventMetadata.getQueryState()));
+        thriftEvent.setUri(eventMetadata.getUri().toString());
+        thriftEvent.setQuery(eventMetadata.getQuery());
+        thriftEvent.setCreate_time_ms(event.getCreateTime().toEpochMilli());
+        thriftEvent.setExecution_start_time_ms(event.getExecutionStartTime().toEpochMilli());
+        thriftEvent.setEnd_time_ms(event.getEndTime().toEpochMilli());
+        thriftEvent.setQueued_time_ms(eventStat.getQueuedTime().toMillis());
+        thriftEvent.setQuery_wall_time_ms(eventStat.getWallTime().toMillis());
+        thriftEvent.setCumulative_memory_bytesecond(eventStat.getCumulativeMemory());
+        thriftEvent.setPeak_memory_bytes(eventStat.getPeakTotalNonRevocableMemoryBytes());
+        thriftEvent.setCpu_time_ms(eventStat.getCpuTime().toMillis());
+        if (eventStat.getAnalysisTime().isPresent()) {
+            thriftEvent.setAnalysis_time_ms(eventStat.getAnalysisTime().get().toMillis());
+        }
+        thriftEvent.setTotal_bytes(eventStat.getTotalBytes());
+        thriftEvent.setQuery_stages(QueryStatsHelper.getQueryStages(eventMetadata));
+        thriftEvent.setOperator_summaries(QueryStatsHelper.getOperatorSummaries(eventStat));
+        thriftEvent.setTotal_rows(eventStat.getTotalRows());
+        thriftEvent.setSplits(eventStat.getCompletedSplits());
+        if (event.getFailureInfo().isPresent()) {
+            QueryFailureInfo eventFailureInfo = event.getFailureInfo().get();
+            thriftEvent.setError_code_id(eventFailureInfo.getErrorCode().getCode());
+            thriftEvent.setError_code_name(eventFailureInfo.getErrorCode().getName());
+            thriftEvent.setFailure_type(eventFailureInfo.getFailureType().orElse(DASH));
+            thriftEvent.setFailure_message(eventFailureInfo.getFailureMessage().orElse(DASH));
+            thriftEvent.setFailure_task(eventFailureInfo.getFailureTask().orElse(DASH));
+            thriftEvent.setFailure_host(eventFailureInfo.getFailureHost().orElse(DASH));
+            thriftEvent.setFailures_json(eventFailureInfo.getFailuresJson());
+        }
+
+        return thriftEvent;
+    }
+}
---
+++ twitter-eventlistener-plugin/src/main/java/com/twitter/presto/plugin/eventlistener/scriber/QueryStatsHelper.java
@@ -0,0 +1,213 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.twitter.presto.plugin.eventlistener.scriber;
+
+import com.facebook.presto.spi.eventlistener.QueryMetadata;
+import com.facebook.presto.spi.eventlistener.QueryStatistics;
+import com.twitter.presto.thriftjava.OperatorStats;
+import com.twitter.presto.thriftjava.QueryStageInfo;
+import io.airlift.log.Logger;
+import io.airlift.units.DataSize;
+import io.airlift.units.Duration;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import javax.json.JsonValue.ValueType;
+
+import java.io.StringReader;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Queue;
+import java.util.stream.Collectors;
+
+public class QueryStatsHelper
+{
+    private static final Logger log = Logger.get(QueryStatsHelper.class);
+
+    private QueryStatsHelper()
+    {
+        throw new AssertionError();
+    }
+
+    private static long getBytesOrNegativeOne(String strVal)
+    {
+        try {
+            return DataSize.valueOf(strVal).toBytes();
+        }
+        catch (IllegalArgumentException e) {
+            log.warn(e,
+                    String.format("Failed to parse io.airlift.units.DataSize '%s', returning -1", strVal));
+            return -1;
+        }
+    }
+
+    private static long getMillisOrNegativeOne(String strVal)
+    {
+        try {
+            return Duration.valueOf(strVal).toMillis();
+        }
+        catch (IllegalArgumentException e) {
+            log.warn(e,
+                    String.format("Failed to parse io.airlift.units.Duration '%s', returning -1", strVal));
+            return -1;
+        }
+    }
+
+    private static QueryStageInfo getQueryStageInfo(int stageId, JsonObject stage)
+    {
+        QueryStageInfo stageInfo = new QueryStageInfo();
+
+        stageInfo.stage_id = stageId;
+        try {
+            JsonObject stageStats = stage.getJsonObject("latestAttemptExecutionInfo").getJsonObject("stats");
+            stageInfo.raw_input_data_size_bytes = getBytesOrNegativeOne(stageStats.getString("rawInputDataSize"));
+            stageInfo.output_data_size_bytes = getBytesOrNegativeOne(stageStats.getString("outputDataSize"));
+            stageInfo.completed_tasks = stageStats.getInt("completedTasks");
+            stageInfo.completed_drivers = stageStats.getInt("completedDrivers");
+            stageInfo.cumulative_memory = stageStats.getJsonNumber("cumulativeUserMemory").doubleValue();
+            stageInfo.peak_memory_reservation_bytes = getBytesOrNegativeOne(stageStats.getString("peakUserMemoryReservation"));
+            stageInfo.total_scheduled_time_millis = getMillisOrNegativeOne(stageStats.getString("totalScheduledTime"));
+            stageInfo.total_cpu_time_millis = getMillisOrNegativeOne(stageStats.getString("totalCpuTime"));
+            stageInfo.total_blocked_time_millis = getMillisOrNegativeOne(stageStats.getString("totalBlockedTime"));
+        }
+        catch (Exception e) {
+            log.error(e, String.format("Error retrieving stage stats for stage %d", stageId));
+            return null;
+        }
+
+        return stageInfo;
+    }
+
+    private static OperatorStats getOperatorStat(String operatorSummaryStr)
+    {
+        try {
+            JsonReader jsonReader = Json.createReader(new StringReader(operatorSummaryStr));
+            return getOperatorStat(jsonReader.readObject());
+        }
+        catch (Exception e) {
+            log.error(e, String.format("Error retrieving operator stats from string:\n%s\n", operatorSummaryStr));
+        }
+
+        return null;
+    }
+
+    private static OperatorStats getOperatorStat(JsonObject obj)
+    {
+        OperatorStats operatorStats = new OperatorStats();
+
+        try {
+            operatorStats.pipeline_id = obj.getInt("pipelineId");
+            operatorStats.operator_id = obj.getInt("operatorId");
+            operatorStats.plan_node_id = obj.getString("planNodeId");
+            operatorStats.operator_type = obj.getString("operatorType");
+            operatorStats.total_drivers = obj.getJsonNumber("totalDrivers").longValue();
+            operatorStats.add_input_calls = obj.getJsonNumber("addInputCalls").longValue();
+            operatorStats.add_input_wall_millis = getMillisOrNegativeOne(obj.getString("addInputWall"));
+            operatorStats.add_input_cpu_millis = getMillisOrNegativeOne(obj.getString("addInputCpu"));
+            operatorStats.input_data_size_bytes = getBytesOrNegativeOne(obj.getString("inputDataSize"));
+            operatorStats.input_positions = obj.getJsonNumber("inputPositions").longValue();
+            operatorStats.sum_squared_input_positions = obj.getJsonNumber("sumSquaredInputPositions").doubleValue();
+            operatorStats.get_output_calls = obj.getJsonNumber("getOutputCalls").longValue();
+            operatorStats.get_output_wall_millis = getMillisOrNegativeOne(obj.getString("getOutputWall"));
+            operatorStats.get_output_cpu_millis = getMillisOrNegativeOne(obj.getString("getOutputCpu"));
+            operatorStats.output_data_size_bytes = getBytesOrNegativeOne(obj.getString("outputDataSize"));
+            operatorStats.output_positions = obj.getJsonNumber("outputPositions").longValue();
+            operatorStats.blocked_wall_millis = getMillisOrNegativeOne(obj.getString("blockedWall"));
+            operatorStats.finish_calls = obj.getJsonNumber("finishCalls").longValue();
+            operatorStats.finish_wall_millis = getMillisOrNegativeOne(obj.getString("finishWall"));
+            operatorStats.finish_cpu_millis = getMillisOrNegativeOne(obj.getString("finishCpu"));
+            operatorStats.memory_reservation_bytes = getBytesOrNegativeOne(obj.getString("userMemoryReservation"));
+            operatorStats.system_memory_reservation_bytes = getBytesOrNegativeOne(obj.getString("systemMemoryReservation"));
+        }
+        catch (Exception e) {
+            log.error(e, String.format("Error retrieving operator stats from JsonObject:\n%s\n", obj.toString()));
+            return null;
+        }
+
+        return operatorStats;
+    }
+
+    public static Map<Integer, QueryStageInfo> getQueryStages(QueryMetadata eventMetadata)
+    {
+        if (!eventMetadata.getPayload().isPresent()) {
+            return null;
+        }
+
+        String payload = eventMetadata.getPayload().get();
+        Queue<JsonObject> stageJsonObjs = new LinkedList<>();
+        try {
+            JsonReader jsonReader = Json.createReader(new StringReader(payload));
+            stageJsonObjs.add(jsonReader.readObject());
+        }
+        catch (Exception e) {
+            log.error(e,
+                    String.format("getQueryStages - Unable to extract JsonObject out of following blob:\n%s\n", payload));
+            return null;
+        }
+
+        Map<Integer, QueryStageInfo> stages = new HashMap<>();
+        while (!stageJsonObjs.isEmpty()) {
+            JsonObject cur = stageJsonObjs.poll();
+            String stageIdStr;
+            try {
+                stageIdStr = cur.getString("stageId");
+                int stageId = Integer.parseInt(stageIdStr.split("\\.")[1]);
+                QueryStageInfo curStage = getQueryStageInfo(stageId, cur);
+                if (curStage != null) {
+                    stages.put(stageId, getQueryStageInfo(stageId, cur));
+                }
+            }
+            catch (Exception e) {
+                log.error(e,
+                        String.format("Failed to parse QueryStageInfo from JsonObject:\n%s\n", cur.toString()));
+                return null;
+            }
+
+            try {
+                cur.getJsonArray("subStages")
+                        .stream()
+                        .filter(val -> val.getValueType() == ValueType.OBJECT)
+                        .forEach(val -> stageJsonObjs.add((JsonObject) val));
+            }
+            catch (Exception e) {
+                log.error(e,
+                        String.format("Failed to get subStages for stage %s, treating as no subStages", stageIdStr));
+            }
+        }
+
+        return stages;
+    }
+
+    public static List<OperatorStats> getOperatorSummaries(QueryStatistics eventStat)
+    {
+        try {
+            return eventStat.getOperatorSummaries()
+                    .stream()
+                    .filter(val -> val != null && !val.isEmpty())
+                    .map(QueryStatsHelper::getOperatorStat)
+                    .filter(Objects::nonNull)
+                    .collect(Collectors.toList());
+        }
+        catch (Exception e) {
+            log.error(e,
+                    String.format("Error converting List<String> to List<OperatorStats>:\n%s\n", eventStat.getOperatorSummaries().toString()));
+        }
+
+        return null;
+    }
+}
