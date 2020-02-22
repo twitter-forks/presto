6c5bab2a9ff4dce93203043034fcd5dab57c403c
Beinan Wang <beinanw@twitter.com>
2020-02-21 22:07:43 -0800

    Fix the null value bug for the columns with the name of sql keywords `

--- presto-hive/src/main/java/com/facebook/presto/hive/parquet/ParquetPageSource.java
+++ presto-hive/src/main/java/com/facebook/presto/hive/parquet/ParquetPageSource.java
@@ -41,7 +41,7 @@ import static com.facebook.presto.hive.HiveColumnHandle.ColumnType.REGULAR;
 import static com.facebook.presto.hive.HiveErrorCode.HIVE_BAD_DATA;
 import static com.facebook.presto.hive.HiveErrorCode.HIVE_CURSOR_ERROR;
 import static com.facebook.presto.hive.parquet.ParquetPageSourceFactory.getParquetType;
-import static com.facebook.presto.parquet.ParquetTypeUtils.lookupColumnByName;
+import static com.facebook.presto.hive.parquet.ParquetTypeUtils.findColumnIObyName;
 import static com.google.common.base.Preconditions.checkState;
 import static java.util.Objects.requireNonNull;
 import static org.apache.parquet.io.ColumnIOConverter.constructField;
@@ -87,7 +87,7 @@ public class ParquetPageSource
 
             if (getParquetType(type, fileSchema, useParquetColumnNames, column, tableName, path).isPresent()) {
                 String columnName = useParquetColumnNames ? name : fileSchema.getFields().get(column.getHiveColumnIndex()).getName();
-                fieldsBuilder.add(constructField(type, lookupColumnByName(messageColumnIO, columnName)));
+                fieldsBuilder.add(constructField(type, findColumnIObyName(messageColumnIO, columnName)));
             }
             else {
                 fieldsBuilder.add(Optional.empty());
