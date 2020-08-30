f247a18d979eaaeda7e79833ff657afa993e00c5
ChunxuTang <1240604020@qq.com>
2020-08-30 14:29:42 -0700

    Move the config manager to singleton to store configs

--- presto-query-predictor/query_predictor/tests/test_config_manager.py
+++ presto-query-predictor/query_predictor/tests/test_config_manager.py
@@ -15,12 +15,24 @@ from query_predictor.predictor.config import TransformerConfigValidator
 
 def test_config_manager(create_transformer_tmp_config):
     config_manager = ConfigManager()
-    config = config_manager.load_config(create_transformer_tmp_config)
+    config_manager_clone = ConfigManager()
 
+    assert (
+        config_manager is config_manager_clone
+    ), "ConfigManager is a singleton class"
+
+    tmp_config_path = create_transformer_tmp_config
+    config = config_manager.load_config(tmp_config_path)
     assert isinstance(config, dict), "Config loaded should be a dictionary"
 
     config_manager.validate(TransformerConfigValidator())
-
     yaml_str = config_manager.serialize_yaml()
-
     assert isinstance(yaml_str, str), "Config serialized should be a str"
+
+    config_clone = config_manager.get_config(tmp_config_path)
+    assert config is config_clone, "get_config should return the config stored"
+
+    config_none = config_manager.get_config("Not existed path")
+    assert (
+        config_none is None
+    ), "get_config should return None if the path does not exist"
